4bfa70d6693a9952f6cdc0d772b3d2a15597005b
Your Name <you@example.com>
2020-08-01 04:56:39 +0000

    omit --global

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,8 @@
 import subprocess
 import sys
 
-subprocess.check_call(
-    ["git", "config", "--local", "user.email", "you@example.com"]
-)
-subprocess.check_call(["git", "config", "--local", "user.name", "Your Name"])
+subprocess.check_call(["git", "config", "user.email", "you@example.com"])
+subprocess.check_call(["git", "config", "user.name", "Your Name"])
 
 
 sys.path.insert(0, "../")
