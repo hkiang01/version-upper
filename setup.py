fbc2276ce05e58ea7994722f006fdc4db8a73a1e
Your Name <you@example.com>
2020-08-05 04:16:25 +0000

    bump minor

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="version-upper",  # Replace with your own username
-    version="0.1.1",
+    version="0.2.0",
     author="Harrison Kiang",
     author_email="hkiang01@gmail.com",
     description="Bump version strings in your repo",
